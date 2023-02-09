251d8b88ad0dcbe273e071fa08e0c23b2bf25a50
Jena Fogarty <Fogartyj7898@gmail.com>
2023-02-09 17:02:07 -0500

    Transfering work to laptop

---
+++ interface/src/components/Steps.js
@@ -0,0 +1,70 @@
+import React, {Component} from "react";
+import socket from '../socketio';
+import Chatbot, {Loading} from 'react-simple-chatbot';
+
+//Plans to seperate this class and it be called within steps
+class Socket extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            bot_uttered: null,
+            loading: true,
+        }
+
+        this.url = "http://localhost:5005/";
+        this.socket = socket(this.url);
+
+    }
+
+    componentDidMount(){
+        const { steps } = this.props;
+        const message = steps.user_uttered.value;
+
+        this.socket.emit('user_uttered', {
+            'message': message,
+            'session_id': this.socket.id,
+        });
+
+        this.socket.on('bot_uttered', (response) => {
+            console.log(response);
+            this.setState({bot_uttered: response.text, loading: false});
+
+        })
+
+        this.setState({loading: true});
+    }
+
+    render() {
+        const {bot_uttered, loading} = this.state;
+        if(loading){
+            return (<div><Loading/></div>)
+        }
+        return (
+            <div>{bot_uttered}</div>
+        )     
+    }
+}
+
+
+const steps =
+[   
+    {
+        id: 'Introduction',
+        message: 'Hi I am Rover.',
+        trigger: 'user_uttered',
+    },
+    {
+        id: 'user_uttered',
+        user: true,
+        trigger: 'bot_uttered',
+    },
+    {
+        id: 'bot_uttered',
+        component: <Socket/>,
+        asMessage: true,
+        trigger: 'user_uttered',
+    }
+];
+
+export default steps;
---
+++ interface/src/components/Theme.js
@@ -0,0 +1,15 @@
+
+
+const theme = {
+    background: '#F9F9F9',
+    //fontFamily: 'Helvetica Neue',
+    headerBgColor: '#EEF2F6',
+    //headerFontColor: '#fff',
+    //headerFontSize: '15px',
+    botBubbleColor: '#D4DBE2',
+    botFontColor: '#000000',
+    userBubbleColor: '#7ED4FF',
+    userFontColor: '#FFFFFF',
+}
+
+export default theme;
