64234fff0a0c08a567c93ab0568b82e1cf75bac8
Ivy1627 <78810686+Ivy1627@users.noreply.github.com>
2023-02-16 07:35:36 -0500

    implemented an alternative cumulative search

--- actions/actions.py
+++ actions/actions.py
@@ -13,6 +13,13 @@ from rasa_sdk import Action, FormValidationAction, Tracker
 from rasa_sdk.events import AllSlotsReset
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.types import DomainDict
+from fuzzywuzzy import process
+from rasa_sdk.events import SlotSet
+from typing import Any, Text, Dict, List
+
+import collections
+import sqlite3
+import random
 
 LOGGER: Final[Logger] = getLogger(__name__)
 STEMMER = SnowballStemmer("english")
@@ -45,7 +52,6 @@ class ValidateJobSearchForm(FormValidationAction):
             )
             return {"title": slot_value}
 
-
 class ActionSearchJobs(Action):
     @dataclass
     class JobSearch:
@@ -65,51 +71,16 @@ class ActionSearchJobs(Action):
         AboutC: str
 
     def name(self) -> str:
-        return "action_search_jobs"
+        return "action_cumulative_search"
 
-    def buildJobSearch(self, tracker: Tracker) -> JobSearch:
-        """Build JobSearch dataclass from filled slots
+    def required_slots(tracker: Tracker) -> List[Text]:
+        return ["job_title", "location"]
 
-        Args:
-            tracker (Tracker): Rasa class that represents the current state of the bots memory
-
-        Returns:
-            JobSearch: dataclass that contains information for searching
-        """
-        return self.JobSearch(title=STEMMER.stem(tracker.get_slot("title")))
-
-    def searchForJobs(self, search: JobSearch) -> Optional[JobPosting]:
-        """Query DB for records matching data from JobSearch; return the first result found
-
-        Args:
-            search (JobSearch): dataclass that contains information for searching
-
-        Returns:
-            JobPosting: all job posting data of first result found; None if not found
-        """
-        for record in data:
-            if search.title in STEMMER.stem(record["Title"].lower()):
-                return self.JobPosting(**record)
-
-        return None
-
-    def outputJobSearch(
-        self, dispatcher: CollectingDispatcher, posting: Optional[JobPosting]
-    ) -> None:
-        """Output important data of a JobPosting
-
-        Args:
-            dispatcher (CollectingDispatcher): Rasa class used to generate responses to send back to user
-            posting (JobPosting): dataclass that contains information from a job posting
-        """
-        if posting:
-            dispatcher.utter_message(
-                text=f"Here is a job matching your criteria:\n{posting.Title},\n{posting.Company},\n{posting.JobDescription}"
-            )
-        else:
-            dispatcher.utter_message(
-                text=f"Sorry, we could not find a job matching your search criteria."
-            )
+    def slot_mappings(self) -> Dict[Text, Any]:
+        return {
+            "title": [self.from_text()],
+            "location": [self.from_text()],
+        }
 
     def run(
         self,
@@ -117,11 +88,81 @@ class ActionSearchJobs(Action):
         tracker: Tracker,
         domain: Dict[str, Any],
     ) -> List[Dict[str, Any]]:
+        """Execute the logic of a function
+        Args: 
+            tracker: object that contains info about the chat history and cuurrent state of the dialogue
+        Returns:
+            a list of SlotSet and/or Event objects
+        """
+        """
+        Runs a query using both the title & location columns (fuzzy matching against the
+        relevent slots). Finds a match for both if possible, otherwise a match for the
+        title only, location only in that order. Output is an utterance directly to the
+        user with a randomly selected matching row.
+        """
+        conn = DbQueryingMethods.create_connection(db_file="./data/jobs.json")
+
+        # get matching entries for title
+        title_value = tracker.get_slot("title")
+        # make sure we don't pass None to our fuzzy matcher
+        if title_value == None:
+            title_value = " "
+        title_name = "Title"
+        title_value = DbQueryingMethods.get_closest_value(conn=conn,
+            slot_name=title_name,slot_value=title_value)[0]
+        query_results_title = DbQueryingMethods.select_by_slot(conn=conn,
+            slot_name=title_name,slot_value=title_value)
+
+        # get matching for location
+        resource_topic_value = tracker.get_slot("location")
+        # make sure we don't pass None to our fuzzy matcher
+        if location_value == None:
+            location_value = " "
+        location_name = "Location"
+        location_value = DbQueryingMethods.get_closest_value(conn=conn,    
+            slot_name=location_name,slot_value=location_value)[0]
+        query_results_location = DbQueryingMethods.select_by_slot(conn=conn,
+            slot_name=location_name,slot_value=location_value)
+
+        # intersection of two queries
+        title_set = collections.Counter(query_results_title)
+        location_set =  collections.Counter(query_results_location)
+
+        query_results_overlap = list((title_set & location_set).elements())
+
+        # apology for not having the right info
+        apology = "I couldn't find exactly what you wanted, but you might like this."
+
+        # return info for both, or topic match or type match or nothing
+        if len(query_results_overlap)>0:
+            return_text = DbQueryingMethods.rows_info_as_text(query_results_overlap)
+        elif len(list(query_results_title))>0:
+            return_text = apology + DbQueryingMethods.rows_info_as_text(query_results_title)
+        elif len(list(query_results_location))>0:
+            return_text = apology + DbQueryingMethods.rows_info_as_text(query_results_location)
+        else:
+            return_text = DbQueryingMethods.rows_info_as_text(query_results_overlap)
+        
+        # print results for user
+        dispatcher.utter_message(text=str(return_text))
+
+        title = tracker.get_slot("title")
+        return [SlotSet("title", title)]
+
+"""
+class AskForLocation(Action):
+    def name(self) -> Text:
+        return "action_ask_location"
+
+    def run(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[str, Any],
+    ) -> List[Dict[str, Any],]:
         search = self.buildJobSearch(tracker=tracker)
         posting = self.searchForJobs(search=search)
         self.outputJobSearch(dispatcher=dispatcher, posting=posting)
-        return []
-
+        location = tracker.get_slot("location")
+        return [SlotSet("location", location)]
+"""
 
 class ResetAllSlots(Action):
     def name(self):
@@ -130,3 +171,61 @@ class ResetAllSlots(Action):
     def run(self, dispatcher, tracker, domain):
         ValidateJobSearchForm.filled_slots = set()
         return [AllSlotsReset()]
+
+
+class DbQueryingMethods:
+    def create_connection(db_file):
+        """ 
+        create a database connection to the SQLite database
+        specified by the db_file
+        :param db_file: database file
+        :return: Connection object or None
+        """
+        conn = None
+        try:
+            conn = sqlite3.connect(db_file)
+        except Error as e:
+            print(e)
+
+        return conn
+
+    def get_closest_value(conn, slot_name, slot_value):
+        """ Given a database column & text input, find the closest 
+        match for the input in the column.
+        """
+        # get a list of all distinct values from our target column
+        fuzzy_match_cur = conn.cursor()
+        fuzzy_match_cur.execute(f"""SELECT DISTINCT {slot_name} 
+                                FROM eduresources""")
+        column_values = fuzzy_match_cur.fetchall()
+
+        top_match = process.extractOne(slot_value, column_values)
+
+        return(top_match[0])
+
+    def select_by_slot(conn, slot_name, slot_value):
+        """
+        Query all rows in the tasks table
+        :param conn: the Connection object
+        :return:
+        """
+        cur = conn.cursor()
+        cur.execute(f'''SELECT * FROM eduresources
+                    WHERE {slot_name}="{slot_value}"''')
+
+        # return an array
+        rows = cur.fetchall()
+
+        return(rows)
+
+    def rows_info_as_text(rows):
+        """
+        Return one of the rows (randomly selcted) passed in 
+        as a human-readable text. If there are no rows, returns
+        text to that effect.
+        """
+        if len(list(rows)) < 1:
+            return "There are no resources matching your query."
+        else:
+            for row in random.sample(rows, 1):
+                return f"Try the {(row[4]).lower()} {row[0]} by {row[1]}. You can find it at {row[2]}."
