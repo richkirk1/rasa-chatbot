2b638b016a183ad7ba20c05373c4df48a2d73ebf
Jena Fogarty <59618911+jenafogarty@users.noreply.github.com>
2023-03-26 13:58:14 -0400

    removed random code

--- interface/src/index.js
+++ interface/src/index.js
@@ -6,17 +6,9 @@ import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-
-
-
 root.render(
     <React.StrictMode>
         <Interface />
     </React.StrictMode>
     
 );
-
-const header = document.getElementById("react-chatbot-kit-chat-header").innerHTML;
-console.log(header);
-header.remove();
-
