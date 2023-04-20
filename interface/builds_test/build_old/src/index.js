0089552f64ccff72152e488e80bd317e9e3e3410
briefchase <chaseglong@gmail.com>
2023-04-20 01:32:10 +0000

    ld ignores

---
+++ interface/builds_test/build_old/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Interface from './Interface';
+
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+
+root.render(
+    <React.StrictMode>
+        <Interface />
+    </React.StrictMode> 
+);
+
